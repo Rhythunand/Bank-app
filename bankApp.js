f6964df6973fec9e5372dcc88d127c430fb0ab59
Rhythunand <rhythunandbiju@gmail.com>
2024-12-06 21:52:23 +0300

    Add files via upload

---
+++ bankApp.js
@@ -0,0 +1,10 @@
+function clearScreen(){
+  var i = document.getElementById("result").value = "";
+}
+function setScreenValue(value){
+  document.getElementById("result").value += value;
+}
+
+function addValue() {
+  document.getElementById("bal").value + i;
+}
